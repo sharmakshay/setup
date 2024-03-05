83afc1f959137c94dd478268e7f646c6dba4be8f
Akshay Sharma <sharmakshay@icloud.com>
2024-03-05 14:40:12 -0500

    added setup for sqlite, geist, vscode, iterm

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# setup
-run `sh init.sh` to setup machine with the following:
-- brew 
+# setup macos for dev
+1. clone repo into root
+2. run `sh setup/init.sh` in root dir to setup machine with the following:
+- brew
 - node (nvm, node, npm -- LTS version)
 - zsh (pure theme + syntax highlighting)
 - python (latest python, poetry, pipx)
+- sqlite
+- geist font
+- vscode (aura theme, custom ui, icons, etc.)
+- iterm2
--- init.sh
+++ init.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+restart_shell() {
+  exec zsh
+}
+
 echo "running setup ..."
-echo "creating dirs ..."
+echo "creating dev dirs ..."
 mkdir ~/Developer
 mkdir ~/Developer/repos
 mkdir ~/Developer/playground
 
 echo "setting up homebrew ..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-exec zsh
+restart_shell
 
 echo "setting up node ..."
 brew install nvm
@@ -15,13 +19,37 @@ nvm install --lts
 node --version
 
 echo "setting up shell ..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 npm install --global pure-prompt
 echo 'autoload -U promptinit; promptinit \n prompt pure' >> .zshrc
 brew install zsh-syntax-highlighting
-echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrcexec zsh
+echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrcrestart_shell
 
 echo "setting up python ..."
 brew install pipx
 brew install python
 alias python=python3
 pipx install poetry
+
+echo "setting up sqlite ..."
+brew install sqlite
+
+echo "setting up geist font ..."
+curl -LOJ https://github.com/vercel/geist-font/releases/download/1.1.0/Geist.Mono.zip
+unzip Geist.Mono.zip
+cp Geist.Mono/*.otf /Library/Fonts/
+
+echo "setting up vscode ..."
+brew install --cask visual-studio-code
+restart_shell
+echo 'export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"' >> .zshrc
+cp setup/configs/vs-settings.json ~/Library/Application Support/Code/User/settings.json
+restart_shell
+code --install-extension drcika.apc-extension
+code --install-extension daltonMenezes.aura-theme
+code --install-extension eamodio.gitlens
+code --install-extension equinusocio.moxer-icons
+code --install-extension esbenp.prettier-vscode
+
+echo "setting up iterm ..."
+brew install --cask iterm2
