91c97e4f0621d5801cf035b2a79c5295fcddccd6
Akshay Sharma <sharmakshay@icloud.com>
2024-03-24 02:07:25 -0400

    refactored each step into its own function, added checks to see if tool already exists (wip)

--- init.sh
+++ init.sh
@@ -3,53 +3,149 @@ restart_shell() {
   exec zsh
 }
 
+exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+setup_dirs() {
+  echo "creating dev dirs ..."
+  dev_dir=~/Developer
+  repos_dir=~/Developer/repos
+  playground_dir=~/Developer/playground
+
+  if [ -d $dev_dir ]
+  then
+    echo "$dev_dir already exists ..."
+  else
+    mkdir $dev_dir
+  fi
+
+  if [ -d  $repos_dir ]
+  then
+    echo "$repos_dir already exists ..."
+  else
+    mkdir $repos_dir
+  fi
+
+  if [ -d $playground_dir ]
+  then
+    echo "$playground_dir already exists ..."
+  else
+    mkdir $playground_dir
+  fi
+}
+
+setup_brew() {
+  if exists brew
+  then
+    echo "homebrew already exists ..."
+  else
+    echo "setting up homebrew ..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    restart_shell
+}
+
+setup_node() {
+  if exists nvm
+  then
+    echo "nvm already exists"
+  else
+    echo "setting up nvm ..."
+    brew install nvm
+
+  if exists node
+  then
+    echo "node already exists"
+  else
+    echo "setting up node ..."
+    nvm install --lts
+}
+
+setup_shell() {
+  echo "setting up shell ..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  npm install --global pure-prompt
+  echo 'autoload -U promptinit; promptinit \n prompt pure' >> .zshrc
+  brew install zsh-syntax-highlighting
+  echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrcrestart_shell
+}
+
+setup_python() {
+  echo "setting up python ..."
+
+  if exists pipx
+  then
+    echo "pipx already exists ..."
+  else
+    brew install pipx
+
+  if exists python
+  then
+    echo "python already exists ..."
+  else
+    brew install python
+
+  if exists python
+  then
+    echo "overriding python cli with python3"
+
+  echo "alias python=python3" >> ~/.zshrc
+
+  if exists poetry
+  then
+    echo "poetry already exists ..."
+  else
+    pipx install poetry
+}
+
+setup_sqlite() {
+  if exists sqlite3
+  then
+    echo "sqlite already exists ..."
+  else
+    echo "setting up sqlite ..."
+    brew install sqlite
+}
+
+setup_font() {
+  echo "setting up geist font ..."
+  curl -LOJ https://github.com/vercel/geist-font/releases/download/1.1.0/Geist.Mono.zip
+  unzip Geist.Mono.zip
+  cp Geist.Mono/*.otf /Library/Fonts/
+}
+
+setup_vscode() {
+  echo "setting up vscode ..."
+  brew install --cask visual-studio-code
+  restart_shell
+  echo 'export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"' >> .zshrc
+  cp setup/configs/vs-settings.json ~/Library/Application Support/Code/User/settings.json
+  restart_shell
+  code --install-extension drcika.apc-extension
+  code --install-extension daltonMenezes.aura-theme
+  code --install-extension eamodio.gitlens
+  code --install-extension equinusocio.moxer-icons
+  code --install-extension esbenp.prettier-vscode
+}
+
+setup_iterm() {
+  echo "setting up iterm ..."
+  brew install --cask iterm2
+}
+
+setup_orbstack() {
+  echo "setting up orbstack (docker alternative) ..."
+  brew install orbstack
+}
+
 echo "running setup ..."
-echo "creating dev dirs ..."
-mkdir ~/Developer
-mkdir ~/Developer/repos
-mkdir ~/Developer/playground
-
-echo "setting up homebrew ..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-restart_shell
-
-echo "setting up node ..."
-brew install nvm
-nvm install --lts
-node --version
-
-echo "setting up shell ..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-npm install --global pure-prompt
-echo 'autoload -U promptinit; promptinit \n prompt pure' >> .zshrc
-brew install zsh-syntax-highlighting
-echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrcrestart_shell
-
-echo "setting up python ..."
-brew install pipx
-brew install python
-alias python=python3
-pipx install poetry
-
-echo "setting up sqlite ..."
-brew install sqlite
-
-echo "setting up geist font ..."
-curl -LOJ https://github.com/vercel/geist-font/releases/download/1.1.0/Geist.Mono.zip
-unzip Geist.Mono.zip
-cp Geist.Mono/*.otf /Library/Fonts/
-
-echo "setting up vscode ..."
-brew install --cask visual-studio-code
-restart_shell
-echo 'export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"' >> .zshrc
-cp setup/configs/vs-settings.json ~/Library/Application Support/Code/User/settings.json
-restart_shell
-code --install-extension drcika.apc-extension
-code --install-extension daltonMenezes.aura-theme
-code --install-extension eamodio.gitlens
-code --install-extension equinusocio.moxer-icons
-code --install-extension esbenp.prettier-vscode
-
-echo "setting up iterm ..."
-brew install --cask iterm2
+setup_dirs
+setup_brew
+setup_node
+setup_shell
+setup_python
+setup_sqlite
+setup_font
+setup_vscode
+setup_iterm
+setup_orbstack
